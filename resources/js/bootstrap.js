ffb41366e33b8eef0e5652325b2adc7b9c55d376
LohYenKuan <lohyenkuan@gmail.com>
2025-02-13 20:16:30 +0800

    app url to vite app url

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,4 +4,4 @@ window.axios = axios;
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 window.axios.defaults.baseURL =
-  import.meta.env.APP_URL || "https://your-production-domain.com";
+  import.meta.env.VITE_APP_URL || "https://your-production-domain.com";
