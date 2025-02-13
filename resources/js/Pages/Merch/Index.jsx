ec1df3611116a168f45851be535b02bebfe247b6
LohYenKuan <lohyenkuan@gmail.com>
2025-02-13 12:30:04 +0800

    Just to test why the nixpacks.toml is not commited to the host

--- resources/js/Pages/Merch/Index.jsx
+++ resources/js/Pages/Merch/Index.jsx
@@ -3,7 +3,7 @@ import AdminLayout from "@/Layouts/AdminLayout";
 export default function Index() {
   return (
     <AdminLayout>
-      <div>Test</div>
+      <div>Coming Soon</div>
     </AdminLayout>
   );
 }
